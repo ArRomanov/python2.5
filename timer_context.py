503b1f6fc7b0f3c8a0c758dccd14132b8a4e6f09
Artem Romanov <romanov-artyom@bk.ru>
2020-05-26 10:15:01 +0300

    Programm time counting for typing on keyboard

--- timer_context.py
+++ timer_context.py
@@ -1,30 +1,46 @@
 import datetime
 import time
+import random
 
 
 class TimerContext:
     def __init__(self):
         self.start_time = time.time()
-        print(f"Время создания контекста: {datetime.datetime.fromtimestamp(self.start_time).strftime('%H:%M:%S')}")
+        print(f"Время старта: {datetime.datetime.fromtimestamp(self.start_time).strftime('%H:%M:%S')}")
 
     def __enter__(self):
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.finish_time = time.time()
-        print(f"Время закрытия контекста: {datetime.datetime.fromtimestamp(self.finish_time).strftime('%H:%M:%S')}")
-        print(f"Контекст работал: {round(self.finish_time - self.start_time, 3)} секунд")
+        print(f"Время завершение: {datetime.datetime.fromtimestamp(self.finish_time).strftime('%H:%M:%S')}")
+        print(f"Общее время: {round(self.finish_time - self.start_time, 3)} секунд")
 
     def print_current_timer(self):
         current_timer = round(time.time() - self.start_time, 3)
-        print(f'Текущий таймер: {current_timer} секунд')
+        print(f'Текущее время: {current_timer} секунд')
 
 
 if __name__ == '__main__':
-    with TimerContext() as timer:
-        time.sleep(5)
-        timer.print_current_timer()
-        time.sleep(1)
-        timer.print_current_timer()
-        time.sleep(2)
-        timer.print_current_timer()
+    phrases = (
+        'Расскажите про покупки! Про какие про покупки? Про покупки, про покупки, про покупочки свои',
+        'Карл у Клары украл рекламу, а Клара у Карла украла бюджет',
+        'Выборка по уборщицам на роллс-ройсах нерепрезентативна',
+        'Кокосовары варят в скорококосоварках кокосовый сок'
+    )
+
+    print('Данная программа считает время набора на клавиатуре предложенного текста')
+    while True:
+        input('Если готовы, то просто нажмите Enter')
+        chosen_phrase = phrases[random.randint(0, len(phrases) - 1)]
+        with TimerContext() as timer:
+            actual_phrase = input(f'{chosen_phrase}\n')
+        try:
+            assert chosen_phrase == actual_phrase
+        except AssertionError:
+            print('Вы допустили одну или несколько ошибок')
+        do_resume = input('Еще раз? y/n\n')
+        if do_resume == 'y':
+            continue
+        else:
+            exit()
